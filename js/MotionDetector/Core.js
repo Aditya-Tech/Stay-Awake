d68c427481a78b69e5b1da60da991eb09755a90b
Aditya Paruchuri <adityaparuchuri@Adityas-MacBook-Pro.local>
2017-08-12 04:21:48 -0400

    added audio files, descriptions/instructions

--- js/MotionDetector/Core.js
+++ js/MotionDetector/Core.js
@@ -135,6 +135,9 @@
 			}
 		}
 
+    function playSound(filename){
+        document.getElementById("sound").innerHTML='<audio autoplay="autoplay"><source src="' + filename + '.mp3" type="audio/mpeg" /><source src="' + filename + '.ogg" type="audio/ogg" /><embed hidden="true" autostart="true" loop="false" src="' + filename +'.mp3" /></audio>';
+            }
 		start = function() {
 
 			$("#webCamWindow").slideDown(1000);
